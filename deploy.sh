a49d9721e47d31cf0a5bb0c8897f84c481e386e8
Tushar Shetty <tusharshetty@gmail.com>
2019-05-19 17:06:41 -0500

    updating the image repo

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #/bin/sh
 
-docker build -t tusharshetty/multi-client:latest -t tusharshetty/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t tusharshetty/multi-server:latest -t tusharshetty/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t tusharshetty/multi-worker:latest -t tusharshetty/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push tusharshetty/multi-client:$SHA
-docker push tusharshetty/multi-client:latest
-docker push tusharshetty/multi-server:$SHA
-docker push tusharshetty/multi-server:latest
-docker push tusharshetty/multi-worker:$SHA
-docker push tusharshetty/multi-worker:latest
+docker build -t kmv834/multi-client:latest -t kmv834/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kmv834/multi-server:latest -t kmv834/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kmv834/multi-worker:latest -t kmv834/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push kmv834/multi-client:$SHA
+docker push kmv834/multi-client:latest
+docker push kmv834/multi-server:$SHA
+docker push kmv834/multi-server:latest
+docker push kmv834/multi-worker:$SHA
+docker push kmv834/multi-worker:latest
 
 kubectl apply -f k8s
 
-kubectl set image deployments/server-deployment server=tusharshetty/multi-server:$SHA
-kubectl set image deployments/client-deployment client=tusharshetty/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=tusharshetty/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=kmv834/multi-server:$SHA
+kubectl set image deployments/client-deployment client=kmv834/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=kmv834/multi-worker:$SHA
